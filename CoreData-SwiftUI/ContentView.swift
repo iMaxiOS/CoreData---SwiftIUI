464ee270cc6435a18fdaa88eabaa8ccb91e3c209
iMakca <ukr-maksim1@yandex.ru>
2019-08-17 12:20:36 +0300

    add project

---
+++ CoreData-SwiftUI/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  CoreData-SwiftUI
+//
+//  Created by Maxim Granchenko on 15.08.2019.
+//  Copyright © 2019 Maxim Granchenko. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+#if DEBUG
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+#endif
---
+++ CoreData-SwiftUI/ToDoItem.swift
@@ -0,0 +1,15 @@
+//
+//  ToDoItem.swift
+//  CoreData-SwiftUI
+//
+//  Created by Maxim Granchenko on 17.08.2019.
+//  Copyright © 2019 Maxim Granchenko. All rights reserved.
+//
+
+import Foundation
+import CoreData
+
+class ToDoItem: NSManagedObject, Identifiable {
+    
+}
+
